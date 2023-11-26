d23182cf4c46a9dc096fbbcda17800fcfb9e5722
Divyansh Jindal <divyanshjindal18@gmail.com>
2023-11-26 14:57:31 +0530

    Added disauthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
+const { AuthRequestMiddleawres } = require('../../middlewares');
 const userRouter = require('./user-routes');
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+router.get('/info', AuthRequestMiddleawres.checkAuth, InfoController.info);
 router.use('/user', userRouter);
 
 module.exports = router;
